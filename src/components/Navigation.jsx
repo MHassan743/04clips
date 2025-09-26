3aebe190ff348a3af3cb72aae2fdcbc1454ccc2a
M Hassan Asghar <hj0889297@gmail.com>
2025-09-26 18:34:53 +0500

    Update Navigation.jsx

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -82,7 +82,7 @@ const Navigation = () => {
                     >
                         <img
                             loading="lazy"
-                            src="src/assets/irmedia.png"
+                            src="../assets/irmedia.png"
                             alt="The Clips Agency"
                             className="nav-logo"
                         />
